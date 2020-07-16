7acb920046667c2c5ee19c9bf035dc87c299f92c
Mohan <mohans@teknuance.com>
2020-07-16 09:50:29 +0530

    loader fix

--- packages/test-my-cli/lib/assert/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const joi = require('../joi');
-
-exports.config = joi.object({
-  debug: joi.debug().optional(),
-  onActivity: joi.func().required()
-}).unknown(true);
--- packages/test-my-cli/lib/assert/meta.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const joi = require('../joi');
-
-exports.meta = joi.object()
-  .pattern(/^[\w-]+$/, joi.any().required())
-  .unknown(false);
